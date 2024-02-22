c205e95ef8a38481835e06075ddc064be51e1f76
Alexandre Mathias <mathias.alexandre@gmail.com>
2024-02-22 10:04:43 +0100

    Project starting code

---
+++ android/app/src/main/kotlin/com/axnd/flutter_todo_hive/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.axnd.flutter_todo_hive
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'app/AppWidget.dart';
+
+void main() {
+  runApp(const AppWidget());
+}
